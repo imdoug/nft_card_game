1af0eee1b457d975992e520ab57b015e4c22f721
Doug Moreira <dougmoreira@Dougs-MBP.lan>
2022-11-03 12:27:34 -0400

    web3/client setup and Higher Order Components added

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { PageHOC } from '../components';
+
+const Home = () => {
+  return (
+    <div>
+       <h1 className='text-white text-xl'>Hello from Home</h1>
+    </div>
+  )
+};
+
+export default PageHOC(
+  Home,
+  <>Welcome to Avax Gods <br/> a Web3 NFT Card Game</>,
+  <>Connect your wallet, to start playing<br/>the ultimate Web3 Battle Card Game</>
+);
